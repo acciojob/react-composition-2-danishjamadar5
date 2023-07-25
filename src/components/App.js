2820c623087fcb25dfe4a3ba2f18dc489e8a0b7c
danish <danishjamadar9089@gmail.com>
2023-07-25 19:33:51 +0530

    submitting code-Tue Jul 25 2023 19:33:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import Modal from "./Modal"
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Modal/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React,{useState} from "react";
+
+const Modal=()=>{
+
+const [status,setStatus]=useState(false)
+    return(
+        <div className="modal-overlay">
+            <button className="modal-p" onClick={()=>setStatus(true)}>Show Modal</button>
+            {
+                status? 
+                <div className="content">
+                <button className="modal-close" onClick={()=>setStatus(false)}>Close Modal</button>
+                <p>This is the content of the modal</p>
+                </div>:
+                null
+
+            }
+            </div>
+
+    )
+}
+
+export default Modal;
